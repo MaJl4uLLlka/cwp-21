02d3e3001f11c8aa2ba6336f6402a18d33496239
MaJl4uLLlka <70745048+MaJl4uLLlka@users.noreply.github.com>
2022-06-27 20:45:21 +0300

    context

--- context/index.js
+++ context/index.js
@@ -10,4 +10,25 @@ module.exports = (Sequelize, config) =>{
     };
 
     const sequelize = new Sequelize(config.db.name, config.db.user, config.db.password, options);
-}
+
+    const Agent = require('../models/agent')(Sequelize, sequelize);
+    const Office = require('../models/office')(Sequelize, sequelize);
+    const Property = require('../models/property')(Sequelize, sequelize);
+
+    //Office -> Agent
+    Agent.belongsTo(Office);
+    Office.hasMany(Agent);
+    
+    //Agent -> Property
+    Property.belongsTo(Agent);
+    Agent.hasMany(Property);
+
+    return {
+        agents: Agent,
+        offices: Office,
+        properties: Property,
+
+        sequelize,
+        Sequelize
+    };
+};
